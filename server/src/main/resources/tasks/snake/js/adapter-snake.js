1de06c8def26069048a841556729d4d252581737
mswiate <marekswiatek1996@gmail.com>
2019-05-25 23:19:14 +0200

    change snake game files

--- server/src/main/resources/tasks/snake/js/adapter-snake.js
+++ server/src/main/resources/tasks/snake/js/adapter-snake.js
@@ -1,7 +1,7 @@
 var config_endpoint = "/snake/config"
 var postScore_endpoint = "/snake/end"
 
-function main(){
+function main() {
     getJSON(config_endpoint, afterConfigFetched);
 }
 
@@ -13,50 +13,73 @@ function getJSON(link, callback) {
     callback(xobj.responseText);
 }
 
-function afterConfigFetched(configJSON){
+function afterConfigFetched(configJSON) {
     console.log("Got response: " + configJSON);
     window.name = configJSON
 }
 
-function sendScoreAndReturnControl(score){
-    var adapterData = JSON.parse(window.name); 
+function sendScoreAndReturnControl(score) {
+    var adapterData = JSON.parse(window.name);
     postScoreJson(postScore_endpoint, score);
 }
 
-function getSpeed(){
+function getSpeed() {
     var age = JSON.parse(window.name)["age"];
-    var speed = Math.round(age / 4);
-	if(speed > 6) {
-		return 6;
-	}
-	if(speed < 1) {
-		return 1;
-	}
-	return speed;
+    var speed = Math.round(age / 5);
+    if (speed > 5) {
+        return 5;
+    }
+    if (speed < 3) {
+        return 3;
+    }
+    return speed;
+}
+
+function getInterval() {
+    var age = JSON.parse(window.name)["age"];
+    if (age < 5)
+        return 7;
+    if (age < 10)
+        return 4;
+    if (age < 15)
+        return 2;
+    return 1;
+}
+
+function getResult() {
+    var age = JSON.parse(window.name)["age"];
+    var result = Math.round(age / 1.6);
+    if (result > 15) {
+        return 15;
+    }
+    if (result < 7) {
+        return 7;
+    }
+    return result;
 }
 
 function postScoreJson(link, score) {
-    var data = JSON.parse(window.name); 
+    var data = JSON.parse(window.name);
 
     var xobj = new XMLHttpRequest();
     xobj.open('POST', link, true);
     xobj.overrideMimeType("application/json");
     xobj.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     xobj.withCredentials = true
-    
-    xobj.onreadystatechange = function() {
+
+    xobj.onreadystatechange = function () {
         if (xobj.readyState === 4 && xobj.status === 200) {
             console.log(xobj.responseText)
             window.location = xobj.responseText
         }
     };
 
-    var sentPayload = 
+    var sentPayload =
         JSON.stringify(
-            {   
-                group : data["group"],
-                nick : data["nick"],
-                age : data["age"],
+            {
+                group: data["group"],
+                nick: data["nick"],
+                age: data["age"],
                 result: score
             }
         );
--- server/src/main/resources/tasks/snake/js/game-snake.js
+++ server/src/main/resources/tasks/snake/js/game-snake.js
@@ -1,367 +1,417 @@
 class SnakePart {
 
-	constructor(x, y) {
-		this.x = x;
-		this.y = y;
-	}
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
 
 }
 
 class Snake {
 
-	constructor(game, x, y, initialPartsAmount) {
+    constructor(game, x, y, initialPartsAmount) {
 
-		this.game = game;
-		this.x = x;
-		this.y = y;
-		this.isAlive = true;
+        this.game = game;
+        this.x = x;
+        this.y = y;
+        this.isAlive = true;
 
-		//Determines if the snake is going up/down or left/right.
-		this.xSpeed = 0;
-		this.ySpeed = 1;
+        //Determines if the snake is going up/down or left/right.
+        this.xSpeed = 0;
+        this.ySpeed = 1;
 
-		//Create the snake's parts.
-		this.parts = [];
-		for (var index = 0; index < initialPartsAmount; index++)
-			this.parts.push(new SnakePart(x-index, y));
+        //Create the snake's parts.
+        this.parts = [];
+        for (var index = 0; index < initialPartsAmount; index++)
+            this.parts.push(new SnakePart(x - index, y));
 
-		//This controls when the snake can change directions.
-		this.canChangeDirection = true;
+        //This controls when the snake can change directions.
+        this.canChangeDirection = true;
 
-		//Captures the key pressed by the player.
-		var _this = this;
-		document.onkeydown = function (event) {
-			_this.controller(event.which);
+        //Captures the key pressed by the player.
+        var _this = this;
+        document.onkeydown = function (event) {
+            _this.controller(event.which);
 
-		};
-		
-		var mc = new Hammer(document.getElementById('stage'));
-		mc.get('swipe').set({ direction: Hammer.DIRECTION_ALL });
-		
-		mc.on("swipeleft swipeup swiperight swipedown", function (ev) {        
-			if (ev.type === 'swipeleft') {
-				_this.controller('left');
-			} else if (ev.type === 'swipeup') {
-				_this.controller('up');
-			} else if (ev.type === 'swiperight') {
-				_this.controller('right');
-			} else if (ev.type === 'swipedown') {
-				_this.controller('down');
-			}
-		});
-	}
+        };
 
-	/*
-		Controll the snake in the canvas.
-		If xSpeed is not equal to 0, tha means the snake
-		is going left or right. If ySpeed is not equal to
-		0, that means the snake is going up or down.
-	*/
-	controller(key) {
-		
-		if (this.game.isPaused) {
-			this.game.isPaused = false;
-			return;
-		}
+        var mc = new Hammer(document.getElementById('stage'));
+        mc.get('swipe').set({direction: Hammer.DIRECTION_ALL});
 
-		if ((key == 37 || key == 'left') && this.ySpeed != 0 && this.canChangeDirection) {
+        mc.on("swipeleft swipeup swiperight swipedown", function (ev) {
+            if (ev.type === 'swipeleft') {
+                _this.controller('left');
+            } else if (ev.type === 'swipeup') {
+                _this.controller('up');
+            } else if (ev.type === 'swiperight') {
+                _this.controller('right');
+            } else if (ev.type === 'swipedown') {
+                _this.controller('down');
+            }
+        });
+    }
 
-			this.canChangeDirection = false;
-			this.xSpeed = -1;
-			this.ySpeed = 0;
+    /*
+        Controll the snake in the canvas.
+        If xSpeed is not equal to 0, tha means the snake
+        is going left or right. If ySpeed is not equal to
+        0, that means the snake is going up or down.
+    */
+    controller(key) {
 
-		}
+        if (this.game.isPaused) {
+            this.game.isPaused = false;
+            return;
+        }
 
-		if ((key == 39 || key == 'right') && this.ySpeed != 0 && this.canChangeDirection) {
+        if ((key == 37 || key == 'left') && this.ySpeed != 0 && this.canChangeDirection) {
 
-			this.canChangeDirection = false;
-			this.xSpeed = 1;
-			this.ySpeed = 0;
+            this.canChangeDirection = false;
+            this.xSpeed = -1;
+            this.ySpeed = 0;
 
-		}
+        }
 
-		if ((key == 38 || key == 'up') && this.xSpeed != 0 && this.canChangeDirection) {
+        if ((key == 39 || key == 'right') && this.ySpeed != 0 && this.canChangeDirection) {
 
-			this.canChangeDirection = false;
-			this.xSpeed = 0;
-			this.ySpeed = -1;
+            this.canChangeDirection = false;
+            this.xSpeed = 1;
+            this.ySpeed = 0;
 
-		}
+        }
 
-		if ((key == 40 || key == 'down') && this.xSpeed != 0 && this.canChangeDirection) {
+        if ((key == 38 || key == 'up') && this.xSpeed != 0 && this.canChangeDirection) {
 
-			this.canChangeDirection = false;
-			this.xSpeed = 0;
-			this.ySpeed = 1;
+            this.canChangeDirection = false;
+            this.xSpeed = 0;
+            this.ySpeed = -1;
 
-		}
+        }
 
-	}
+        if ((key == 40 || key == 'down') && this.xSpeed != 0 && this.canChangeDirection) {
 
-	//Add a new part to the end of the snake.
-	addPart() {
+            this.canChangeDirection = false;
+            this.xSpeed = 0;
+            this.ySpeed = 1;
 
-		var lastPart = this.parts[this.parts.length - 1];
-		this.parts.push(new SnakePart(lastPart.x, lastPart.y));
+        }
 
-	}
+    }
 
-	//Update the snake in the canvas.
-	update() {
-		this.x += this.xSpeed;
-		this.y += this.ySpeed;
+    //Add a new part to the end of the snake.
+    addPart() {
 
-		if (this.x > this.game.width - 1)
-			this.die();
-		if (this.x < 0) {
-			this.die();
-		}
-		if (this.y > this.game.height - 1)
-			this.die();
-		if (this.y < 0) {
-			this.die();
-		}
+        var lastPart = this.parts[this.parts.length - 1];
+        this.parts.push(new SnakePart(lastPart.x, lastPart.y));
 
-		//Renders each part in the canvas starting by the last part.
-		console.log(this.parts.length)
-		for (var index = this.parts.length - 1; index >= 0; index--) {
+    }
 
-			var part = this.parts[index];
+    //Update the snake in the canvas.
+    update() {
+        this.x += this.xSpeed;
+        this.y += this.ySpeed;
 
-			if (index != 0)
-			{
+        if (this.x > this.game.width - 1)
+            this.die();
+        if (this.x < 0) {
+            this.die();
+        }
+        if (this.y > this.game.height - 1)
+            this.die();
+        if (this.y < 0) {
+            this.die();
+        }
+        if (this.game.poison.isOnPoison(this.x, this.y)) {
+            this.die();
+        }
 
-				part.x = this.parts[index - 1].x;
-				part.y = this.parts[index - 1].y;
+        //Renders each part in the canvas starting by the last part.
+        for (var index = this.parts.length - 1; index >= 0; index--) {
 
-				//If the heads touches any part of the body it will die.
-				if (this.x == part.x && this.y == part.y) {
-					this.die();
-				}
+            var part = this.parts[index];
 
-			}
-			else {
+            if (index != 0) {
 
-				part.x = this.x;
-				part.y = this.y;
+                part.x = this.parts[index - 1].x;
+                part.y = this.parts[index - 1].y;
 
-			}
+                //If the heads touches any part of the body it will die.
+                if (this.x == part.x && this.y == part.y) {
+                    this.die();
+                }
 
-			this.game.grid.fillTile(part.x, part.y, "#111410");
+            } else {
 
-		}
+                part.x = this.x;
+                part.y = this.y;
 
-		//After each update we must allow the snake to change direction again.
-		this.canChangeDirection = true;
+            }
 
-	}
+            this.game.grid.fillTile(part.x, part.y, "#111410");
 
-	//The snake is dead. That's not a big surprise.
-	die() {
+        }
 
-		this.isAlive = false;
+        //After each update we must allow the snake to change direction again.
+        this.canChangeDirection = true;
 
-	}
+    }
+
+    //The snake is dead. That's not a big surprise.
+    die() {
+
+        this.isAlive = false;
+
+    }
 
 }
 
 class Food {
 
-	constructor(game) {
+    constructor(game) {
 
-		this.game = game;
-		this.placeFood();
+        this.game = game;
+        this.placeFood();
 
-	}
+    }
 
-	//Places a food in a random tile in the grid.
-	placeFood() {
+    //Places a food in a random tile in the grid.
+    placeFood() {
+        do {
+            this.x = Math.floor(Math.random() * (this.game.width - 2)) + 1;
+            this.y = Math.floor(Math.random() * (this.game.height - 2)) + 1;
+        } while (this.game.poison.isOnPoison(this.x, this.y));
 
-		this.x = Math.floor(Math.random() * this.game.width);
-		this.y = Math.floor(Math.random() * this.game.height);
+    }
 
-	}
+    //Render the food in the grid.
+    update() {
 
-	//Render the food in the grid.
-	update() {
+        this.game.grid.fillTile(this.x, this.y, "#3dc427");
 
-		this.game.grid.fillTile(this.x, this.y, "#0d66c4");
+    }
 
-	}
+}
+
+class Poison {
+
+
+    constructor(game) {
+        this.positions = [];
+        this.game = game;
+        this.placePoison();
+
+    }
+
+    //Places a poison in a random tile in the grid.
+    placePoison() {
+        console.log(this.game.score)
+        console.log(this.game.spawn)
+        if (this.game.score % this.game.spawn !== 0)
+            return;
+        do {
+            var x = Math.floor(Math.random() * this.game.width);
+            var y = Math.floor(Math.random() * this.game.height);
+
+        } while (Math.abs(this.game.snake.x - x) < 3 && (Math.abs(this.game.snake.y - y) < 3));
+        this.positions.push({x: x, y: y})
+    }
+
+    isOnPoison(x, y) {
+        for (var i = 0; i < this.positions.length; i++) {
+            var position = this.positions[i];
+            if (position.x === x && position.y === y)
+                return true;
+        }
+        return false;
+    }
+
+    //Render the poison in the grid.
+    update() {
+        for (var i = 0; i < this.positions.length; i++) {
+            var position = this.positions[i];
+            this.game.grid.fillTile(position.x, position.y, "#c41a2c");
+        }
+
+    }
 
 }
 
 class RenderGrid {
 
-	constructor(game) {
+    constructor(game) {
 
-		this.game = game;
-		this.grid = [];
+        this.game = game;
+        this.grid = [];
 
-		this.buildGrid();
+        this.buildGrid();
 
-	}
+    }
 
-	buildGrid() {
-	
-		//Loop through all the rows of the grid.
-		for (var x = 0; x < this.game.width; x++) {
+    buildGrid() {
 
-			this.grid[x] = [];
+        //Loop through all the rows of the grid.
+        for (var x = 0; x < this.game.width; x++) {
 
-			//Loop through all the columns of the grid.
-			for (var y = 0; y < this.game.height; y++) {
+            this.grid[x] = [];
 
-				//Create a tile to add to the grid.
-				var divTile = document.createElement("div");
-				divTile.id = "cell";
-				divTile.style.position = "absolute";
-				divTile.style.width = divTile.style.height = this.game.size + "px";
-				divTile.style.left = x * this.game.size + "px";
-				divTile.style.top = y * this.game.size + "px";
+            //Loop through all the columns of the grid.
+            for (var y = 0; y < this.game.height; y++) {
 
-				//Add the tile to the front end grid.
-				this.game.divStage.appendChild(divTile);
+                //Create a tile to add to the grid.
+                var divTile = document.createElement("div");
+                divTile.id = "cell";
+                divTile.style.position = "absolute";
+                divTile.style.width = divTile.style.height = this.game.size + "px";
+                divTile.style.left = x * this.game.size + "px";
+                divTile.style.top = y * this.game.size + "px";
 
-				/*
-				Add the tile to the grid array.
-				The isFilled property determines
-				if the tile must be filled with a color or not.
-				*/
-				this.grid[x][y] = {
-					div: divTile,
-					isFilled: false,
-					color: "white"
-				};
+                //Add the tile to the front end grid.
+                this.game.divStage.appendChild(divTile);
 
-			}
+                /*
+                Add the tile to the grid array.
+                The isFilled property determines
+                if the tile must be filled with a color or not.
+                */
+                this.grid[x][y] = {
+                    div: divTile,
+                    isFilled: false,
+                    color: "white"
+                };
 
-		}
+            }
 
-	}
+        }
 
-	//Mark a tile as filled with some color.
-	fillTile(x, y, color) {
+    }
 
-		if (this.grid[x]) {
+    //Mark a tile as filled with some color.
+    fillTile(x, y, color) {
 
-			if (this.grid[x][y]) {
+        if (this.grid[x]) {
 
-				var tile = this.grid[x][y];
+            if (this.grid[x][y]) {
 
-				tile.isFilled = true;
-				tile.color = color;
+                var tile = this.grid[x][y];
 
-			}
+                tile.isFilled = true;
+                tile.color = color;
 
-		}
+            }
 
-	}
+        }
 
-	update() {
+    }
 
-		//Loop through all the rows of the grid.
-		for (var x = 0; x < this.game.width; x++) {
+    update() {
 
-			//Loop through all the columns of the grid.
-			for (var y = 0; y < this.game.height; y++) {
+        //Loop through all the rows of the grid.
+        for (var x = 0; x < this.game.width; x++) {
 
-				var tile = this.grid[x][y];
-				var newBackgroundColor = tile.isFilled ? tile.color : "#9AC805";
-				tile.div.style.background = newBackgroundColor;
+            //Loop through all the columns of the grid.
+            for (var y = 0; y < this.game.height; y++) {
 
-				/*
-				Need to be reseted, so in the
-				next update it will
-				be able to change colors if necessary.
-				*/
-				tile.isFilled = false;
+                var tile = this.grid[x][y];
+                var newBackgroundColor = tile.isFilled ? tile.color : "#ffffff";
+                tile.div.style.background = newBackgroundColor;
 
-			}
+                /*
+                Need to be reseted, so in the
+                next update it will
+                be able to change colors if necessary.
+                */
+                tile.isFilled = false;
 
-		}
+            }
 
-	}
+        }
+
+    }
 
 }
 
 class Game {
 
-	constructor(divStageId, spanScoreId) {
+    constructor(divStageId, spanScoreId) {
 
-		this.width = 10;
-		this.height = 12;
-		this.size = 30;
-		this.fps = getSpeed();
-		this.isPaused = true;
-		this.rendered = false;
+        this.spawn = getInterval();
+        this.width = 10;
+        this.height = 12;
+        this.size = 30;
+        this.fps = getSpeed();
+        this.isPaused = true;
+        this.rendered = false;
 
-		this.divStage = document.getElementById(divStageId);
-		this.spanScore = document.getElementById(spanScoreId);
+        this.divStage = document.getElementById(divStageId);
+        this.spanScore = document.getElementById(spanScoreId);
 
-		this.score = 0;
-		this.maxScore = 15;
-		this.grid = new RenderGrid(this);
-		this.food = new Food(this);
-		this.snake = new Snake(this, 5, 2, 3);
+        this.score = 0;
+        this.maxScore = getResult();
+        this.grid = new RenderGrid(this);
+        this.snake = new Snake(this, 5, 2, 3);
+        this.poison = new Poison(this);
+        this.food = new Food(this);
 
-		//Start loop.
-		var _this = this;
-		this.interval = setInterval(function () {
-			_this.update();
-		}, 1000/this.fps);
+        //Start loop.
+        var _this = this;
+        this.interval = setInterval(function () {
+            _this.update();
+        }, 1000 / this.fps);
 
-	}
+    }
 
-	update() {
+    update() {
 
-		if (this.isPaused) {
-			if(!this.rendered) {
-				this.rendered = true;
-				this.food.update();
-				this.snake.update();
-				this.grid.update();
-			}
-			return;
-		}
+        if (this.isPaused) {
+            if (!this.rendered) {
+                this.rendered = true;
+                this.food.update();
+                this.poison.update();
+                this.snake.update();
+                this.grid.update();
+            }
+            return;
+        }
 
-		this.spanScore.innerHTML = this.score;
+        this.spanScore.innerHTML = ' ' + this.score + '/' + this.maxScore;
 
-		if(this.score >= this.maxScore) {
-			this.spanScore.style.color = 'green';
-			this.spanScore.innerHTML = 'wygrałeś :)';
-			sendScoreAndReturnControl(this.score / this.maxScore);
-		}
-		
-		if (! this.snake.isAlive) {
-			this.spanScore.style.color = 'red';
-			this.spanScore.innerHTML = 'przegrałeś :(';
-			var scoreTmp = (this.score / this.maxScore) - 0.5;
-			if(scoreTmp < 0) {
-				scoreTmp = 0;
-			}
-			sendScoreAndReturnControl(scoreTmp);
-		}
+        if (this.score >= this.maxScore) {
+            this.spanScore.style.color = 'green';
+            this.spanScore.innerHTML = 'wygrałeś :)';
+            sendScoreAndReturnControl(this.score / this.maxScore);
+        }
 
-		this.food.update();
-		this.snake.update();
 
-		//If the snake its the food, it grows. Just like real live.
-		if (this.snake.x == this.food.x && this.snake.y == this.food.y) {
+        this.food.update();
+        this.poison.update();
+        this.snake.update();
 
-			this.food.placeFood();
-			this.snake.addPart();
-			this.score++;
-		}
+        if (!this.snake.isAlive) {
+            this.spanScore.style.color = 'red';
+            this.spanScore.innerHTML = 'przegrałeś :(';
+            var scoreTmp = (this.score / this.maxScore) - 0.5;
+            if (scoreTmp < 0) {
+                scoreTmp = 0;
+            }
+            sendScoreAndReturnControl(scoreTmp);
+        }
+        //If the snake its the food, it grows. Just like real live.
+        if (this.snake.x == this.food.x && this.snake.y == this.food.y) {
 
-		this.grid.update();
-	}
-	
-	changeDirection(direction) {
-		this.snake.controller(direction);
-	}
+            this.score++;
+            this.food.placeFood();
+            this.poison.placePoison();
+            this.snake.addPart();
+        }
+
+        this.grid.update();
+    }
+
+    changeDirection(direction) {
+        this.snake.controller(direction);
+    }
 }
 
 function changeDirection(direction, game) {
-	game.changeDirection(direction);
+    game.changeDirection(direction);
 }
